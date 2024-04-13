503f6f3f2a59d4120fd7f762f842d2eb8040771c
FredBeck <capitaine.fred@gmail.com>
2024-04-13 10:33:41 +0200

    Prevent opening multiple main form
    Using scriptcontext.sticky to keep track of the form

--- root.py
+++ root.py
@@ -1,4 +1,3 @@
-
 """
 root file of rhino_awesome_pave.
 Its only job is to make an instance of the main UI and add
@@ -11,21 +10,47 @@ from ui import main
 from ui import keymanager
 from functions import handler
 
+
+import Rhino
+import scriptcontext as sc
+
 reload(main)
 reload(keymanager)
 reload(handler)
 
-import Rhino
-
 main.Form.H = handler.Handler
 
+
+# The form will be kept track of in the scriptcontext sticky under this key
+MAIN_FORM_STICY = "awesome_pave_main_form"
+
+
+# Whenever the form is closed, remove it from the scriptcontext sticky
+def OnClosed(sender, args):
+    if MAIN_FORM_STICY in sc.sticky:
+        del sc.sticky[MAIN_FORM_STICY]
+
+
 def rhino_awesome_pave():
+
+    # Do nothing if the form is visible
+    if MAIN_FORM_STICY in sc.sticky: return
+
+    # Create a new form
     form = main.Form()
     form.Owner = Rhino.UI.RhinoEtoApp.MainWindow
+    # Hook to the Closed event
+    form.Closed += OnClosed
     form.Show()
+
     kpm = keymanager.KeyPressManager()
     kpm.addIncreaseCallback( form.handleIncrease )
     kpm.addDecreaseCallback( form.handleDecrease )
 
+    # Keep the ref in the scriptcontext sticky
+    sc.sticky[MAIN_FORM_STICY] = form
+
+
 if __name__ == '__main__':
     rhino_awesome_pave()
+    
